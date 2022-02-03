c3905f3bda2424ddda3313a4ee649088918a3102
mia <cocoball200@gmail.com>
2022-02-03 23:44:10 +0900

    add: initial setting

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-slate-400 py-10 px-5">
+      <div className="rounded-2xl bg-white p-10">Taildwind do not work T0T</div>
+      <div className="rounded-2xl bg-white p-10"></div>
+      <div className="rounded-2xl bg-white p-10"></div>
+      <div className="rounded-2xl bg-white p-10"></div>
+    </div>
+  );
+};
+
+export default Home;
